310f303b3cc4127c2927b3cd1bf299c03e143aff
Tabitha Chapman <tabbychapman@gmail.com>
2015-03-28 01:07:41 -0700

    indents!

--- assets/js/path.js
+++ assets/js/path.js
@@ -1,25 +1,25 @@
-jQuery(document).ready( function($) {
+jQuery(document).ready(function ($) {
 
     // Initial path data
-    var path = { debug: true };
+    var path = {debug: true};
 
-	// Change default beat tick period
-    wp.heartbeat.interval( 'fast' ); // slow (1 beat every 60 seconds), standard (1 beat every 15 seconds), fast (1 beat every 5 seconds)
+    // Change default beat tick period
+    wp.heartbeat.interval('fast'); // slow (1 beat every 60 seconds), standard (1 beat every 15 seconds), fast (1 beat every 5 seconds)
 
     // Initiate namespace with path data
-    wp.heartbeat.enqueue( 'path', path, false );
+    wp.heartbeat.enqueue('path', path, false);
 
     // Hook into the heartbeat-send
-    jQuery(document).on('heartbeat-send.path', function(e, data) {
+    jQuery(document).on('heartbeat-send.path', function (e, data) {
 
         // Send data to Heartbeat
-        if ( data.hasOwnProperty( 'path' ) ) {
+        if (data.hasOwnProperty('path')) {
 
-            if ( data.path.debug === 'true' ) {
+            if (data.path.debug === 'true') {
 
-                console.log( 'Data Sent: ' );
+                console.log('Data Sent: ');
                 console.log(data);
-                console.log( '------------------' );
+                console.log('------------------');
 
             } // End If Statement
 
@@ -28,23 +28,23 @@ jQuery(document).ready( function($) {
     });
 
     // Listen for the custom event "heartbeat-tick" on $(document).
-    jQuery(document).on( 'heartbeat-tick.path', function(e, data) {
+    jQuery(document).on('heartbeat-tick.path', function (e, data) {
 
         // Receive Data back from Heartbeat
-        if ( data.hasOwnProperty( 'path' ) ) {
+        if (data.hasOwnProperty('path')) {
 
-            if ( data.path.debug === 'true' ) {
+            if (data.path.debug === 'true') {
 
-                console.log( 'Data Received: ' );
+                console.log('Data Received: ');
                 console.log(data);
-                console.log( '------------------' );
+                console.log('------------------');
 
             } // End If Statement
 
         } // End If Statement
 
         // Pass data back into namespace
-        wp.heartbeat.enqueue( 'path', data.path, false );
+        wp.heartbeat.enqueue('path', data.path, false);
 
     });
 
--- heartbeat-api-path.php
+++ heartbeat-api-path.php
@@ -1,11 +1,12 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
 /**
  * Heartbeat API Path Class
 
  */
-class Heartbeat_API_Path {
+class Heartbeat_API_Path
+{
     /**
      * @var string
      */
@@ -19,48 +20,51 @@ class Heartbeat_API_Path {
      */
     public $version;
 
-	/**
-	 * Constructor.
-	 * @since  1.0.0
-	 * @return  void
-	 */
-	public function __construct ( $file ) {
-		// Class variables
+    /**
+     * Constructor.
+     * @since  1.0.0
+     * @return  void
+     */
+    public function __construct($file)
+    {
+        // Class variables
         $this->token = 'heartbeat-api-path';
-		$this->plugin_url = trailingslashit( plugins_url( '', $file ) );
+        $this->plugin_url = trailingslashit(plugins_url('', $file));
 
         // Actions & filters
-		add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue_styles' ) );
-    	add_action( 'wp_footer', array( &$this, 'enqueue_scripts' ) );
-    	add_filter( 'heartbeat_settings', array( &$this, 'heartbeat_settings' ) );
-        add_action( 'wp_dashboard_setup',  array( &$this, 'example_add_dashboard_widgets' ) );
-        add_action( 'wp_print_footer_scripts', array( &$this, 'path_heartbeat_footer_js' ),20 );
-        add_action( 'admin_print_footer_scripts', array( &$this, 'path_admin_heartbeat_footer_js' ),20 );
+        add_action('wp_enqueue_scripts', array(&$this, 'enqueue_styles'));
+        add_action('wp_footer', array(&$this, 'enqueue_scripts'));
+        add_filter('heartbeat_settings', array(&$this, 'heartbeat_settings'));
+        add_action('wp_dashboard_setup', array(&$this, 'example_add_dashboard_widgets'));
+        add_action('wp_print_footer_scripts', array(&$this, 'path_heartbeat_footer_js'), 20);
+        add_action('admin_print_footer_scripts', array(&$this, 'path_admin_heartbeat_footer_js'), 20);
 
-	} // End __construct()
+    } // End __construct()
 
-	/**
-	 * Initialise the code.
-	 * @since  1.0.0
-	 * @return void
-	 */
-	public function init () {
+    /**
+     * Initialise the code.
+     * @since  1.0.0
+     * @return void
+     */
+    public function init()
+    {
 
         // Heartbeat filters
-		add_filter( 'heartbeat_received', array( &$this, 'respond_to_browser_authenticated' ), 5, 2 );
-		add_filter( 'heartbeat_nopriv_received', array( &$this, 'respond_to_browser_unauthenticated' ), 5, 2 );
+        add_filter('heartbeat_received', array(&$this, 'respond_to_browser_authenticated'), 5, 2);
+        add_filter('heartbeat_nopriv_received', array(&$this, 'respond_to_browser_unauthenticated'), 5, 2);
 
-	} // End init()
+    } // End init()
 
-	/**
+    /**
      * Enqueue frontend JavaScripts.
      * @since  1.0.0
      * @return void
      */
-    public function enqueue_scripts () {
+    public function enqueue_scripts()
+    {
 
         // Load the path javascript
-        wp_enqueue_script( $this->token . '-path' , $this->plugin_url . 'assets/js/path.js', array( 'jquery', 'heartbeat' ), '1.0.0', true );
+        wp_enqueue_script($this->token . '-path', $this->plugin_url . 'assets/js/path.js', array('jquery', 'heartbeat'), '1.0.0', true);
 
     } // End enqueue_scripts()
 
@@ -69,11 +73,12 @@ class Heartbeat_API_Path {
      * @since  1.0.0
      * @return void
      */
-    public function enqueue_styles () {
+    public function enqueue_styles()
+    {
 
         // Load the path frontend CSS
-        wp_register_style( $this->token . '-frontend', $this->plugin_url . 'assets/css/frontend.css', '', '1.0.0', 'screen' );
-        wp_enqueue_style( $this->token . '-frontend' );
+        wp_register_style($this->token . '-frontend', $this->plugin_url . 'assets/css/frontend.css', '', '1.0.0', 'screen');
+        wp_enqueue_style($this->token . '-frontend');
 
     } // End enqueue_styles()
 
@@ -82,31 +87,33 @@ class Heartbeat_API_Path {
      * @since  1.0.0
      * @return void
      */
-    public function heartbeat_settings( $settings ) {
+    public function heartbeat_settings($settings)
+    {
 
-    	$settings['interval'] = "fast";
-    	return $settings;
+        $settings['interval'] = "fast";
+        return $settings;
 
-	} // End heartbeat_settings
+    } // End heartbeat_settings
 
     /**
      * Handle send data and respond to the browser.
      * @since  1.0.0
      * @return $response data to the heartbeat tick function
      */
-	public function respond_to_browser_unauthenticated( $response, $data ) {
+    public function respond_to_browser_unauthenticated($response, $data)
+    {
 
         $guestPath = get_option("guest_path");
 
 
         $currentGuest = $guestPath[$data['ipaddress']];
-        if(count($currentGuest) >=30){
+        if (count($currentGuest) >= 30) {
             array_pop($currentGuest);
         }
-        $guestPath[$data['ipaddress']][] = array("time"=>$data['sent_time'],"page_uri"=>$data['current_page_uri']);
+        $guestPath[$data['ipaddress']][] = array("time" => $data['sent_time'], "page_uri" => $data['current_page_uri']);
 
 
-        update_option("guest_path",$guestPath);
+        update_option("guest_path", $guestPath);
 
         // Do custom code here
         $data['greeting'] = 'Hey You! ';
@@ -115,34 +122,35 @@ class Heartbeat_API_Path {
 
         return $response;
 
-	} // End respond_to_browser_unauthenticated()
+    } // End respond_to_browser_unauthenticated()
 
     /**
      * Handle authenticated user (logged in) send data and respond to the browser.
      * @since  1.0.0
      * @return $response data to the heartbeat tick function
      */
-    public function respond_to_browser_authenticated( $response, $data ) {
+    public function respond_to_browser_authenticated($response, $data)
+    {
 
         $guestPath = get_option("guest_path");
 
         $path = '';
         $cnt = 0;
-        foreach($guestPath as $key=>$paths){
-            $path .= "<h2>".$key."</h2>";
-            $path .="<ol>";
+        foreach ($guestPath as $key => $paths) {
+            $path .= "<h2>" . $key . "</h2>";
+            $path .= "<ol>";
 
-            foreach($paths as $p){
-                if($p['page_uri']!=$prevUri){
-                    $path .="<li>". $p['page_uri']."</li>";
-                    if($cnt > 10){
+            foreach ($paths as $p) {
+                if ($p['page_uri'] != $prevUri) {
+                    $path .= "<li>" . $p['page_uri'] . "</li>";
+                    if ($cnt > 10) {
                         break;
                     }
                     $cnt++;
                 }
                 $prevUri = $p['page_uri'];
             }
-            $path .="</ol>";
+            $path .= "</ol>";
 
 
         }
@@ -155,24 +163,27 @@ class Heartbeat_API_Path {
         return $response;
 
     } // End respond_to_browser_authenticated()
+
     /**
      * Add a widget to the dashboard.
      *
      * This function is hooked into the 'wp_dashboard_setup' action below.
      */
-    function example_add_dashboard_widgets() {
+    function example_add_dashboard_widgets()
+    {
 
         wp_add_dashboard_widget(
             'example_dashboard_widget',         // Widget slug.
             'SimpleLytics - Real-Time-ish Analytics',         // Title.
-            array(&$this,'example_dashboard_widget_function') // Display function.
+            array(&$this, 'example_dashboard_widget_function') // Display function.
         );
     }
 
     /**
      * Create the function to output the contents of our Dashboard Widget.
      */
-    function example_dashboard_widget_function() {
+    function example_dashboard_widget_function()
+    {
 
         // Display whatever it is you want to show.
         echo "<div id='simpleLytics'>Please wait for analytics to start.</div>";
@@ -182,24 +193,28 @@ class Heartbeat_API_Path {
     /**
      *
      */
-    function path_heartbeat_footer_js() {
-      ?>
+    function path_heartbeat_footer_js()
+    {
+        ?>
         <script>
-            (function($){
+            (function ($) {
                 var landingServerTime;
 
-                $(document).on('heartbeat-send', function(e, data) {
+                $(document).on('heartbeat-send', function (e, data) {
                     data['path_heartbeat'] = 'dashboard_summary';
                     data['current_page_uri'] = "<?php echo $_SERVER['REQUEST_URI']?>";
-                    data['send_time'] = Math.round(e.timeStamp/1000);
+                    data['send_time'] = Math.round(e.timeStamp / 1000);
                     data['ipaddress'] = "<?php echo $_SERVER['REMOTE_ADDR']?>";
                 });
 
                 // Listen for the custom event "heartbeat-tick" on $(document).
-                $(document).on( 'heartbeat-tick', function(e, data) {
-                    if(!landingServerTime){landingServerTime = data['server_time']};
-                    if(data['server_time'] > (landingServerTime + 30)){
-                        alert(data['greeting'] +'Did you know that '+ (data['server_time']- landingServerTime) + ' seconds passed just now? Welcome Back!');
+                $(document).on('heartbeat-tick', function (e, data) {
+                    if (!landingServerTime) {
+                        landingServerTime = data['server_time']
+                    }
+                    ;
+                    if (data['server_time'] > (landingServerTime + 30)) {
+                        alert(data['greeting'] + 'Did you know that ' + (data['server_time'] - landingServerTime) + ' seconds passed just now? Welcome Back!');
                         landingServerTime = (data['server_time']);
 
                     }
@@ -214,37 +229,39 @@ class Heartbeat_API_Path {
     /**
      *
      */
-    function path_admin_heartbeat_footer_js(){
+    function path_admin_heartbeat_footer_js()
+    {
         global $pagenow;
 
         // Only proceed if on the dashboard
-        if( 'index.php' != $pagenow )
+        if ('index.php' != $pagenow)
             return;
         ?>
         <script>
-            (function($){
+            (function ($) {
 
                 // Hook into the heartbeat-send
-                $(document).on('heartbeat-send', function(e, data) {
+                $(document).on('heartbeat-send', function (e, data) {
                     data['path_heartbeat'] = 'dashboard_summary';
                 });
 
                 // Listen for the custom event "heartbeat-tick" on $(document).
-                $(document).on( 'heartbeat-tick', function(e, data) {
+                $(document).on('heartbeat-tick', function (e, data) {
 
                     // Only proceed if our EDD data is present
-                    if ( ! data['data_to_display'] )
+                    if (!data['data_to_display'])
                         return;
 
                     // Log the response for easy proof it works
-                    console.log( data['data_to_display'] );
+                    console.log(data['data_to_display']);
 
                     // Update sale count and bold it to provide a highlight
-                    $('#example_dashboard_widget #simpleLytics').html( data['data_to_display'] ).css( 'font-weight', 'bold' );
+                    $('#example_dashboard_widget #simpleLytics').html(data['data_to_display']).css('font-weight', 'bold');
 
                     // Return font-weight to normal after 2 seconds
-                    setTimeout(function(){
-                        $('#example_dashboard_widget #simpleLytics').css( 'font-weight', 'normal' );;
+                    setTimeout(function () {
+                        $('#example_dashboard_widget #simpleLytics').css('font-weight', 'normal');
+                        ;
                     }, 2000);
 
                 });
@@ -253,7 +270,7 @@ class Heartbeat_API_Path {
 
 
 
-        <?php
+    <?php
     }
 
 } // End Heartbeat_API_Path
